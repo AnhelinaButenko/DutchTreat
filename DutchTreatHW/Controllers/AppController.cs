4964873c4d9805009e46ca7787789ab6fa3ff56a
Angelina Butenko <angelinaa2712@gmail.com>
2023-09-04 23:20:33 +0300

    using asp.net mvc

--- DutchTreatHW/Controllers/AppController.cs
+++ DutchTreatHW/Controllers/AppController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreatHW.Services;
+using DutchTreatHW.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 
 namespace DutchTreatHW.Controllers
 {
     public class AppController : Controller
     {
+        private readonly INullMailService _mailService;
+
+        public AppController(INullMailService mailService) 
+        {
+           _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -13,17 +22,25 @@ namespace DutchTreatHW.Controllers
         [HttpGet("contact")]
         public IActionResult Contact()
         {
-            ViewBag.Title = "Contact Us";
-
-            throw new InvalidOperationException("Bad things happen");
-
             return View();
         }
 
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                // Send the email
+                _mailService.SendMessage("shawn@wildermich.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+			}
+
+			return View();
+		}
+
         public IActionResult About() 
         {
-            ViewBag.Title = "About Us";
-
             return View();
         }
     }
---
+++ DutchTreatHW/Services/INullMailService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace DutchTreatHW.Services
+{
+    public interface INullMailService
+    {
+        ILogger<NullMailService> Logger { get; }
+
+        void SendMessage(string to, string subject, string body);
+    }
+}
